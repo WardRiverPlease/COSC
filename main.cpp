c4a8748810923e70c7a44c66daf63d79b716b023
WardRiverPlease <89794173+WardRiverPlease@users.noreply.github.com>
2024-04-27 06:38:00 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,26 +5,37 @@
 using namespace std;
 
 int main(){
-    srand(time(NULL));
-    GameBoard gameboard(13,13);
+   // srand(time(NULL));
+    GameBoard gameboard(15,15);
     gameboard.draw_board();
+ 
 
     char direction;
     bool game_over = false;
 
+    
+
     while(!game_over){
+        
         direction = _getch();
-        if (direction == 'W' || direction == 'w' || direction == 'A' || direction == 'a' ||
-            direction == 'S' || direction == 's' || direction == 'D' || direction == 'd') {
+        if (direction == 'W' || direction == 'w' || direction == 'S' || direction == 's' ||
+            direction == 'D' || direction == 'd' || direction == 'A' || direction == 'a') {
             // Move the snake
             gameboard.getSnake().move(direction);
             
             // Redraw the game board with the updated snake position
+            
             gameboard.draw_board();
         } else if (direction == 'Q' || direction == 'q') {
             // Quit the game if the user presses 'Q'
             game_over = true;
         }
+        if(gameboard.checkCollisionWithFood()){
+            gameboard.getSnake().grow();
+         //   gameboard.placeFood();
+        }
+
+      //  gameboard.draw_board();
     }
 
 
