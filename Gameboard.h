7418d42fc5058d4227fc4a167abfb7068d0152a2
WardRiverPlease <89794173+WardRiverPlease@users.noreply.github.com>
2024-04-29 09:27:58 -0500

    Update Gameboard.h

--- Gameboard.h
+++ Gameboard.h
@@ -2,6 +2,8 @@
 #define GAMEBOARD_H
 
 #include "Snake.h"
+#include "LevelManager.h"
+
 #include <vector>
 #include <utility>
 
@@ -9,29 +11,33 @@ using namespace std;
 
 class GameBoard {
 private:
+ 
     int width;
     int height;
     pair<int, int> food_position;
     pair<int, int> bonus_position;
     vector<vector<char>> board;
-    
+    bool bonusSpawned;
     Snake snake;
+
+
     
 public:
     bool checkCollisionWithFood();
+    bool checkCollisionWithBonus();
+    void setBonusSpawned(bool value);
     int getWidth() const;
     int getHeight() const ;
     GameBoard(int w, int h);
     void placeFood();
     void placeBonus();
     void initializeBoundaries();
-    void draw_board();
-    // Additional methods to update snake position, food position, etc.
-    //Snake Classes
+    void draw_board(int score, int level);
     Snake& getSnake();
+    
 
 
  
 };
 
-#endif // GAMEBOARD_H
+#endif 
