2e4d23d7f2ddc74b29c3dbf2c6818fc02dfbecac
WardRiverPlease <89794173+WardRiverPlease@users.noreply.github.com>
2024-04-29 23:36:59 -0500

    Update DoubleScore.cpp

--- DoubleScore.cpp
+++ DoubleScore.cpp
@@ -2,7 +2,7 @@
 
 DoubleScore::DoubleScore() : active(false) {}
 
-void DoubleScore::applyPower(int score) {
+void DoubleScore::applyPower(int& score) {
     // Apply the power-up effect to the score (double the score per food item)
     if(isActive()){
         score *= 2;
